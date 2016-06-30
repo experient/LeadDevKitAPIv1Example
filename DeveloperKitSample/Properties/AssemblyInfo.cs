2735dcb7e7545ec5df49d66ca6062ebcf19dc376
Automated Deployment System <noreply@experient-inc.com>
2016-06-30 14:15:23 -0400

    Deployment of DevKitSample v5.0.15752.19416 to Prod:MAIN by GitHub Deployment Plugin

--- DeveloperKitSample/Properties/AssemblyInfo.cs
+++ DeveloperKitSample/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("4.40.0.0")]
-[assembly: AssemblyVersion("4.40.0.0")]
-[assembly: AssemblyFileVersion("4.40.15654.19318")]
+// [assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.15752.19416")]
