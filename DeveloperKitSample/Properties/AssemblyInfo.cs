c2f69f8d4729632b47ac2909b9e4659ba318d264
Automated Deployment System <noreply@experient-inc.com>
2019-02-21 21:17:12 -0500

    Deployment of DevKitSample v5.22.0.450 to PROD:MAIN by GitHub Deployment Plugin

--- DeveloperKitSample/Properties/AssemblyInfo.cs
+++ DeveloperKitSample/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Experient Inc a Maritz Travel Company")]
 [assembly: AssemblyProduct("Real Time Data API")]
-[assembly: AssemblyCopyright("Copyright © 2018 Experient Inc")]
+[assembly: AssemblyCopyright("Copyright © 2019 Experient Inc")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("5.20.0.0")]
-[assembly: AssemblyVersion("5.20.0.0")]
-[assembly: AssemblyFileVersion("5.20.0.25060")]
+// [assembly: AssemblyVersion("5.22.0.0")]
+[assembly: AssemblyVersion("5.22.0.0")]
+[assembly: AssemblyFileVersion("5.22.0.450")]
