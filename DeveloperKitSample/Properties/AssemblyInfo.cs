a0e85d2906e45d47ed0685b6372aa6bea874a802
Automated Deployment System <noreply@experient-inc.com>
2022-10-18 22:02:13 -0400

    Deployment of DevKitSample v5.25.0.41676 to PROD:MAIN by GitHub Deployment Plugin

--- DeveloperKitSample/Properties/AssemblyInfo.cs
+++ DeveloperKitSample/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("5.25.0.0")]
 [assembly: AssemblyVersion("5.25.0.0")]
-[assembly: AssemblyFileVersion("5.25.0.40015")]
+[assembly: AssemblyFileVersion("5.25.0.41676")]
